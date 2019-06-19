7c802965bd2c1112a6186dc89b032f0019f0c15f
Anna Spysz <anna.spysz@stackery.io>
2019-06-19 13:27:35 -0700

    Testing

--- src/eradicateVPC/index.js
+++ src/eradicateVPC/index.js
@@ -8,15 +8,17 @@ exports.handler = async () => {
   const regions = ['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'sa-east-1'];
   // We'll be using promises to make sure everything gets deleted
   let promiseArray = [];
-
+  // be sure to add AWSConfigRole to function Policies
   const configParams = {
     'includeDeletedResources': false,
-    'resourceType': 'AWS::EC2::VPC'
+    'resourceType': 'AWS::DynamoDB::Table' // AWS::EC2::VPC
   }
 
   try {
     const config = await cs.listDiscoveredResources(configParams).promise();
+    const configCount = await cs.getDiscoveredResourceCounts().promise();
     console.log(config);
+    console.log(configCount);
   } catch (error) {
     console.log(error);
   }
