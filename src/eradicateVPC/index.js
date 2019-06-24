445fa4ca282955115d81ff2db81b1fca63ad3ff1
Anna Spysz <anna.spysz@stackery.io>
2019-06-24 11:12:23 -0700

    Expanding function for dependencies

--- src/eradicateVPC/index.js
+++ src/eradicateVPC/index.js
@@ -1,26 +1,15 @@
 const aws = require('aws-sdk');
 
 exports.handler = async () => {
-  const cs = new aws.ConfigService();
-
-  const resourceParams = {
-    ConfigurationAggregatorName: 'vpc-eradicator', /* required */
-    ResourceType: 'AWS::EC2::VPC' // replace this with whatever resource you want deleted
-  };
+  // List of resources related to VPCs in AWS. Note: the actual AWS::EC2::VPC needs to be last in the array, as AWS won't let you delete VPCs with existing dependencies
+  // See these fun docs for an explanation: https://aws.amazon.com/premiumsupport/knowledge-center/troubleshoot-dependency-error-delete-vpc/
+  const resourcesToEradicate = ['AWS::EC2::EIP', 'AWS::EC2::InternetGateway', 'AWS::EC2::NetworkAcl', 'AWS::EC2::NetworkInterface', 'AWS::EC2::RouteTable', 'AWS::EC2::SecurityGroup', 'AWS::EC2::Subnet', 'AWS::EC2::VPC'] // replace this with whatever resources you want deleted
 
   try {
-    // Get array of resources for eradication from AWS Config
-    const resources = await cs.listAggregateDiscoveredResources(resourceParams).promise();
-
-    if (resources.ResourceIdentifiers.length > 0) { // run eradicateResources function only if there is a VPC
-      let VPC = resources.ResourceIdentifiers.length === 1 ? 'VPC' : 'VPCs'; // good grammar is important
-      console.log(`Oh noes! ${resources.ResourceIdentifiers.length} ${VPC} discovered!`);
-
-      await eradicateResources(resources);
-    } else {
-      console.log('No VPCs found here, your money is safe!');
+    for (let resource of resourcesToEradicate) {
+      console.log(`Beginning eradication on ${resource}`);
+      await findResources(resource);
     }
-
   } catch (error) {
     console.log(error);
   }
@@ -28,7 +17,29 @@ exports.handler = async () => {
   return {};
 };
 
-async function eradicateResources(resources) {
+async function findResources (resource) {
+  const cs = new aws.ConfigService();
+
+  const resourceParams = {
+    ConfigurationAggregatorName: 'vpc-eradicator', /* required */
+    ResourceType: resource
+  };
+
+  // Get array of resources for eradication from AWS Config
+  const resources = await cs.listAggregateDiscoveredResources(resourceParams).promise();
+  console.log(resources);
+
+  if (resources.ResourceIdentifiers.length > 0) { // run eradicateResources function only if the resource was found
+    let count = resources.ResourceIdentifiers.length === 1 ? 'VPC resource' : 'VPC resources'; // good grammar is important
+    console.log(`Oh noes! ${resources.ResourceIdentifiers.length} ${count} discovered! Running eradicator.`);
+    // Run eradicateResources function on each instance of that resource
+    // await eradicateResources(resources);
+  } else {
+    console.log(`No ${resource} found here, your money is safe for now!`);
+  }
+}
+
+async function eradicateResource(resource) {
   const ec2 = new aws.EC2();
   // We'll be using promises to make sure everything gets deleted
   let promiseArray = [];
